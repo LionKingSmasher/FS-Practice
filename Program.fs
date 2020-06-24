ff75e8f654083a5c7dc768a0d57b099e01d24fdf
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-06-24 20:12:17 +0900

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,33 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let add() = 
+    let first_num = int64(Console.ReadLine());
+    let second_num = int64(Console.ReadLine());
+    first_num + second_num
+
+let sub() = 
+    let first_num = int64(Console.ReadLine());
+    let second_num = int64(Console.ReadLine());
+    first_num - second_num
+
+let mul() =
+    let first_num = int64(Console.ReadLine());
+    let second_num = int64(Console.ReadLine());
+    first_num * second_num
+
+let find(n) = 
+    match n with
+    | "+" -> printfn "%d" (add())
+    | "-" -> printfn "%d" (sub())
+    | "*" -> printfn "%d" (mul())
+    |_ -> printfn "다시 입력하세요"
+
+[<EntryPoint>]
+
+let main argv =
+    while true do
+        let input_calc = System.Console.ReadLine();
+        find(input_calc)
+    0 // return an integer exit code
