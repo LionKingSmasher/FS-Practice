5fd0883656993a44fc898a8a14d56ed646f05e0f
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-07-01 08:39:46 +0900

    umm...
    F# 소스코드 추가질

---
+++ Cal.fs
@@ -0,0 +1,30 @@
+﻿module Cal
+
+let add(first_num, second_num) = 
+    first_num + second_num
+
+let sub(first_num, second_num) = 
+    first_num - second_num
+
+let mul(first_num, second_num) =
+    first_num * second_num
+
+let div(first_num, second_num) =
+    first_num / second_num
+
+let rec fac(num) =
+    if num = 1 then
+        1
+    else 
+        num * fac(num)
+
+
+(*
+let find(n) = 
+    match n with
+    | "+" -> printfn "%d" (add())
+    | "-" -> printfn "%d" (sub())
+    | "*" -> printfn "%d" (mul())
+    | "/" -> printfn "%d" (div())
+    |_ -> printfn "다시 입력하세요"
+ *)
--- Program.fs
+++ Program.fs
@@ -1,33 +1,28 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open Cal
 
-open System
-
-let add() = 
-    let first_num = int64(Console.ReadLine());
-    let second_num = int64(Console.ReadLine());
-    first_num + second_num
-
-let sub() = 
-    let first_num = int64(Console.ReadLine());
-    let second_num = int64(Console.ReadLine());
-    first_num - second_num
-
-let mul() =
-    let first_num = int64(Console.ReadLine());
-    let second_num = int64(Console.ReadLine());
-    first_num * second_num
-
-let find(n) = 
+let find(n, a, b) = 
     match n with
-    | "+" -> printfn "%d" (add())
-    | "-" -> printfn "%d" (sub())
-    | "*" -> printfn "%d" (mul())
+    | "+" -> printfn "%d" (add(int(a), int(b)))
+    | "-" -> printfn "%d" (sub(int(a), int(b)))
+    | "*" -> printfn "%d" (mul(int(a), int(b)))
+    | "/" -> printfn "%d" (div(int(a), int(b)))
     |_ -> printfn "다시 입력하세요"
+
+let find_2(n, a) =
+    match n with
+    | "fac" -> printfn "%d" (fac(int(a)))
+    |_ -> printfn "다시 입력하세요"
+
 
 [<EntryPoint>]
 
 let main argv =
     while true do
-        let input_calc = System.Console.ReadLine();
-        find(input_calc)
+        printfn "부호를 입력하세요"
+        let n = System.Console.ReadLine()
+        printfn "수를 입력하세요"
+        let i = System.Console.ReadLine()
+        find_2(n, i)
+        //find(input_calc)
     0 // return an integer exit code
